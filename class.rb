9b24935895e9a305caa8890bc4f93bf69e6cc2b8
bafraikin <fraikin.baptiste@gmail.com>
2018-04-19 11:57:02 +0200

    on a bien avancé

--- class.rb
+++ class.rb
@@ -4,7 +4,6 @@ class Boardcase
   def initialize
     @value = ""
   end
-
 end
 
 class Board
@@ -28,7 +27,7 @@ class Board
         char = "\s"
         unless @plateau[y][:"#{x}"].value == "" then char = @plateau[y][:"#{x}"].value end
         print "\s#{char}\s"
-        if x < 2 then print "|" end
+        if x < 2 then print "|" end 
       end
     end
     print "\n\n"
@@ -36,20 +35,35 @@ class Board
 
   def check_plateau
 
-    if line
+    3.times do | nb | 
+      unless  plateau[nb][:"0"].value == ""
+        if  plateau[nb][:"0"].value ==  plateau[nb][:"1"].value && plateau[nb][:"0"].value == plateau[nb][:"2"].value 
+          return  plateau[nb][:"0"].value
+        end
+      end
     end
-
-    if column
+    3.times do | nb | 
+      unless  plateau[0][:"#{nb}"].value == ""
+        if plateau[0][:"#{nb}"].value == plateau[1][:"#{nb}"].value && plateau[0][:"#{nb}"].value == plateau[2][:"#{nb}"].value
+          return  plateau[0][:"#{nb}"].value
+        end
+      end
     end
-
-    if diago
+    unless plateau[1][:"1"].value == ""
+      if  plateau[0][:"0"].value == plateau[1][:"1"].value && plateau[1][:"1"].value == plateau[2][:"2"].value
+        return plateau[1][:"1"].value
+      end
+      if  plateau[0][:"2"].value == plateau[1][:"1"].value && plateau[1][:"1"].value == plateau[2][:"0"].value
+        return plateau[1][:"1"].value
+      end
     end
-
+    ""
   end
 end
 
 class Game
-  attr_accessor :player, :board, :turn
+  attr_accessor :player, :board, :turn, :play
+
 
   def initialize
     @player = []
@@ -57,28 +71,42 @@ class Game
     @player[1] = Player.new("X")
     @board = Board.new
     @turn = 0
+    @play = 0
   end
 
   def next_turn
-    @turn +=1
-
+    line = 0
+    column = 0
+    while line < 1 || line > 3
+      print "choisis la ligne où tu veux jouer\n"
+      line = gets.chomps.to_i
+    end
+    line -=1
+    while column < 1 || column > 3
+      print "choisis la colonne où tu veux jouer\n"
+      column = gets.chomps.to_i
+    end
+    column -=1
+   @play == 0 ? @play +=1 : @play -=1
+   @plateau.check_plateau != "" ? "" : "" 
+     @turn += 1
   end
 
+  def c_gagne(symbol)
+  end
 end
 
 
 class Player
   attr_accessor :name, :symbol
+
   def initialize(symbol)
+    print "\nrentre ici ton nom : "
     @name = gets.chomp.to_s
     @symbol = symbol
   end
 end
 
+coucou = Game.new
 coucou.board.puts_plateau
-coucou.board.plateau[0][:"0"].value = "X"
-coucou.board.plateau[1][:"1"].value = "X"
-coucou.board.plateau[2][:"2"].value = "X"
-coucou.board.puts_plateau
-
 
